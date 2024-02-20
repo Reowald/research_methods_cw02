a2483c4be60ec0b968df9514256c8f1ffbd91abe
Reowald <ryandanielbyrne@gmail.com>
2024-02-20 09:07:36 +0000

    pseudo code

---
+++ kolmogorov_classifier.py
@@ -0,0 +1,16 @@
+import os
+
+# create a dictionay with linearised values
+def kolmogorov_model_trainer(input_values):
+    model = {}
+    for value in input values:
+        linearise_values(value)
+        model.insert(category, linearise_values)
+
+# create a classifier that can import new data and make a prediction
+def kolmogorov_classifier(model):  # You can adjust the threshold as needed
+    file_size = os.path.getsize(file_path)
+    
+    if file_size ~ linerised_value: #R^2 or neaest distance
+        return classification
+
